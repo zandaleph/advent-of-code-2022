3ab7b21e14c646b776c4a051d270917c3f755ee6
Zack Spellman <zandaleph@gmail.com>
2022-12-24 17:42:05 -0800

    Remove unused helper method

--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -14,8 +14,3 @@ fun readInput(name: String) = File("src/main/resources", "$name.txt")
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
-
-/**
- * The cleaner shorthand for printing output.
- */
-fun Any?.println() = println(this)
