aaaa06aa0571f0b73ee6729c749ab0f77628a9a1
Zack Spellman <zandaleph@gmail.com>
2022-12-25 22:18:30 -0800

    Day 08 part 2

--- src/main/kotlin/Day08.kt
+++ src/main/kotlin/Day08.kt
@@ -2,6 +2,7 @@ class Day08 : Solution<Int> {
 
     companion object {
         private const val TEST_OUTPUT = 21
+        private const val TEST_OUTPUT_2 = 8
     }
 
     private data class ViewState(
@@ -10,10 +11,10 @@ class Day08 : Solution<Int> {
     )
 
     override val part1 = SolutionPart(TEST_OUTPUT) { lines ->
-        val numCols = lines.size
-        val numRows = lines.first().length
-        check(lines.all { it.length == numRows })
-        views(numCols, numRows).fold(setOf<Pair<Int, Int>>()) { seen, view ->
+        val numRows = lines.size
+        val numCols = lines.first().length
+        check(lines.all { it.length == numCols })
+        views(numRows, numCols).fold(setOf<Pair<Int, Int>>()) { seen, view ->
             view.fold(ViewState()) { state, pair ->
                 val tree = lines[pair.first][pair.second]
                 if (tree > state.tallest) {
@@ -23,20 +24,40 @@ class Day08 : Solution<Int> {
         }.size
     }
 
-    private fun views(cols: Int, rows: Int): Sequence<Sequence<Pair<Int, Int>>> = sequence {
-        (0 until cols).forEach { col ->
-            val steps = (0 until rows).map { row -> col to row }
+    private fun views(rows: Int, cols: Int): Sequence<Sequence<Pair<Int, Int>>> = sequence {
+        (0 until rows).forEach { row ->
+            val steps = (0 until cols).map { col -> row to col }
             yield(steps.asSequence())
             yield(steps.asReversed().asSequence())
         }
-        (0 until rows).forEach { row ->
-            val steps = (0 until cols).map { col -> col to row }
+        (0 until cols).forEach { col ->
+            val steps = (0 until rows).map { row -> row to col }
             yield(steps.asSequence())
             yield(steps.asReversed().asSequence())
         }
     }
 
-    override val part2 = SolutionPart { 0 }
+    override val part2 = SolutionPart(TEST_OUTPUT_2) { lines ->
+        val numRows = lines.size
+        val numCols = lines.first().length
+        check(lines.all { it.length == numCols })
+        (1 until (numRows - 1)).asSequence().flatMap { row ->
+            (1 until (numCols - 1)).asSequence().map { col ->
+                val curHeight = lines[row][col]
+                viewsFrom(row to col, numRows, numCols).fold(1) { acc, view ->
+                    val shorter = view.takeWhile { lines[it.first][it.second] < curHeight }.count()
+                    acc * (shorter + if (shorter < view.size) 1 else 0)
+                }
+            }
+        }.max()
+    }
+
+    private fun viewsFrom(point: Pair<Int, Int>, rows: Int, cols: Int): List<List<Pair<Int, Int>>> = listOf(
+        IntProgression.fromClosedRange(point.first - 1, 0, -1).map { it to point.second },
+        IntProgression.fromClosedRange(point.second - 1, 0, -1).map { point.first to it },
+        IntProgression.fromClosedRange(point.first + 1, rows - 1, 1).map { it to point.second },
+        IntProgression.fromClosedRange(point.second + 1, cols - 1, 1).map { point.first to it },
+    )
 }
 
-fun main() = solutionMain<Day08>()
+fun main() = solutionMain<Day08>(true)
