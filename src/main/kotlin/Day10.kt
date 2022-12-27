7c3ca51806dda753537c459ccc13030adf1bf271
Zack Spellman <zandaleph@gmail.com>
2022-12-26 20:09:36 -0800

    Day 10 part 2

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -1,13 +1,25 @@
-class Day10 : Solution<Int> {
+import kotlin.math.abs
+
+class Day10 : Solution<String> {
 
     companion object {
-        private const val TEST_OUTPUT = 13140
+        private const val TEST_OUTPUT = "13140"
+        private val TEST_OUTPUT_2 = """
+            ##..##..##..##..##..##..##..##..##..##..
+            ###...###...###...###...###...###...###.
+            ####....####....####....####....####....
+            #####.....#####.....#####.....#####.....
+            ######......######......######......####
+            #######.......#######.......#######.....
+        """.trimIndent().replace('.', ' ').replace('#', '█')
 
         private const val FIRST_MEASURE_CYCLE = 20
         private const val LAST_MEASURE_CYCLE = 220
         private const val MEASURE_CYCLE_STEP = 40
         private val measureCycles =
             IntProgression.fromClosedRange(FIRST_MEASURE_CYCLE, LAST_MEASURE_CYCLE, MEASURE_CYCLE_STEP)
+
+        private const val TOTAL_CYCLES = 240
 
         private const val NOOP_CODE = "noop"
         private const val ADDX_CODE_PREFIX = "addx "
@@ -32,27 +44,34 @@ class Day10 : Solution<Int> {
         val x: Int = 1,
     )
 
-    override val part1 = SolutionPart(TEST_OUTPUT) { lines ->
-        val instructions = lines.asSequence().map { line ->
+    private fun cpuStates(lines: List<String>) = sequence<CpuState> {
+        lines.asSequence().map { line ->
             when {
                 line.startsWith(NOOP_CODE) -> Noop
                 line.startsWith(ADDX_CODE_PREFIX) -> AddX(line.removePrefix(ADDX_CODE_PREFIX).toInt())
                 else -> error("Invalid code: $line")
             }
+        }.fold(CpuState()) { state, inst ->
+            repeat(inst.cycles) { yield(state) }
+            inst.operation(state)
         }
-        val cpuStates = sequence {
-            instructions.fold(CpuState()) { state, inst ->
-                repeat(inst.cycles) { yield(state) }
-                inst.operation(state)
-            }
-        }
-        cpuStates.filterIndexed { idx, _ -> measureCycles.contains(idx + 1) }
-            .zip(measureCycles.asSequence())
-            .map { (state, cycle) -> state.x * cycle }
-            .sum()
     }
 
-    override val part2 = SolutionPart { 0 }
+    override val part1 = SolutionPart(TEST_OUTPUT) { lines ->
+        cpuStates(lines).filterIndexed { idx, _ -> measureCycles.contains(idx + 1) }
+            .zip(measureCycles.asSequence())
+            .map { (state, cycle) -> state.x * cycle }
+            .sum().toString()
+    }
+
+    override val part2 = SolutionPart(TEST_OUTPUT_2) { lines ->
+        cpuStates(lines).take(TOTAL_CYCLES)
+            .chunked(MEASURE_CYCLE_STEP)
+            .joinToString(separator = "\n") {
+                it.mapIndexed { idx, state -> if (abs(idx - state.x) <= 1) '█' else ' ' }
+                    .joinToString(separator = "")
+            }
+    }
 }
 
 fun main() = solutionMain<Day10>()
