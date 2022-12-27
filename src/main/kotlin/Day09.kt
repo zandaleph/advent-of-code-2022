b2cb1d56db1d7ef2598f587ad1a5c110db82549d
Zack Spellman <zandaleph@gmail.com>
2022-12-26 17:05:41 -0800

    Day 09 part 2

--- src/main/kotlin/Day09.kt
+++ src/main/kotlin/Day09.kt
@@ -4,14 +4,22 @@ class Day09 : Solution<Int> {
 
     companion object {
         private const val TEST_OUTPUT = 13
+        private const val TEST_OUTPUT_2 = 1
+
+        private const val ROPE_LEN_PART_1 = 2
+        private const val ROPE_LEN_PART_2 = 10
     }
 
     private data class RopeState(
-        val head: Pair<Int, Int> = 0 to 0,
-        val tail: Pair<Int, Int> = head,
-        val visited: Set<Pair<Int, Int>> = setOf(tail),
+        val rope: List<Pair<Int, Int>>,
+        val visited: Set<Pair<Int, Int>> = setOf(rope.last()),
     ) {
+        companion object {
+            fun ofLength(len: Int) = RopeState(List(len) { 0 to 0 })
+        }
+
         fun move(heading: String): RopeState {
+            val head = rope.first()
             val newHead = when (heading) {
                 "L" -> head.copy(second = head.second - 1)
                 "R" -> head.copy(second = head.second + 1)
@@ -19,15 +27,18 @@ class Day09 : Solution<Int> {
                 "U" -> head.copy(first = head.first + 1)
                 else -> error("Invalid heading: $heading")
             }
-            val newTail = updateTail(newHead, tail)
-            return copy(head = newHead, tail = newTail, visited = visited + newTail)
+            val newRope = mutableListOf(newHead)
+            rope.subList(1, rope.size).forEach {
+                newRope.add(updateTail(newRope.last(), it))
+            }
+            return copy(rope = newRope, visited = visited + newRope.last())
         }
 
         private fun updateTail(head: Pair<Int, Int>, tail: Pair<Int, Int>): Pair<Int, Int> {
             val rowDiff = head.first - tail.first
             val colDiff = head.second - tail.second
             return if (abs(rowDiff) > 1 || abs(colDiff) > 1) {
-                tail.copy(first = tail.first + rowDiff.direction(), second = tail.second + colDiff.direction())
+                (tail.first + rowDiff.direction()) to (tail.second + colDiff.direction())
             } else tail
         }
 
@@ -36,14 +47,15 @@ class Day09 : Solution<Int> {
         }
     }
 
-    override val part1 = SolutionPart(TEST_OUTPUT) { lines ->
-        lines.fold(RopeState()) { state, line ->
+    private fun solve(lines: List<String>, initialState: RopeState) =
+        lines.fold(initialState) { state, line ->
             val (heading, steps) = line.split(' ').let { it[0] to it[1].toInt() }
             (1..steps).fold(state) { s, _ -> s.move(heading) }
         }.visited.size
-    }
 
-    override val part2 = SolutionPart { 0 }
+    override val part1 = SolutionPart(TEST_OUTPUT) { solve(it, RopeState.ofLength(ROPE_LEN_PART_1)) }
+
+    override val part2 = SolutionPart(TEST_OUTPUT_2) { solve(it, RopeState.ofLength(ROPE_LEN_PART_2)) }
 }
 
 fun main() = solutionMain<Day09>()
