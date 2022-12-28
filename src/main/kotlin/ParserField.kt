bfd61a2a602e7016fe93295e11a14efe07e8920e
Zack Spellman <zandaleph@gmail.com>
2022-12-27 19:01:42 -0800

    Simplify - don't need to override equals/hashCode

--- src/main/kotlin/ParserField.kt
+++ src/main/kotlin/ParserField.kt
@@ -1,7 +1,4 @@
 class ParserField<T : Any>(
     val pattern: String,
     val parser: String.() -> T,
-) {
-    override fun equals(other: Any?) = this === other
-    override fun hashCode() = super.hashCode()
-}
+)
