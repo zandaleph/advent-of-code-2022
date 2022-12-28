b2a3ded71e663a113af5ba9f6e2e9160e5674e8c
Zack Spellman <zandaleph@gmail.com>
2022-12-28 07:13:53 -0800

    Day 12 part 2

--- src/main/kotlin/Day12.kt
+++ src/main/kotlin/Day12.kt
@@ -1,53 +1,54 @@
 import java.util.LinkedList
 import java.util.Queue
 
+typealias Coord = Pair<Int, Int>
+
 class Day12 : Solution<Int> {
 
     companion object {
         private const val TEST_OUTPUT = 31
+        private const val TEST_OUTPUT_2 = 29
     }
 
-    private operator fun <T> List<List<T>>.get(coords: Pair<Int, Int>): T? =
-        this[coords.first][coords.second]
+    private operator fun <T> List<List<T>>.get(coord: Coord): T? =
+        this[coord.first][coord.second]
 
-    private operator fun MutableList<MutableList<Int?>>.set(coords: Pair<Int, Int>, value: Int?) {
-        this[coords.first][coords.second] = value
+    private operator fun MutableList<MutableList<Int?>>.set(coord: Coord, value: Int?) {
+        this[coord.first][coord.second] = value
     }
 
-    override val part1 = SolutionPart(TEST_OUTPUT) { lines ->
-        val terrain = lines.map { it.map { c -> if (c == 'S') 'a' - 1 else c } }
+    private fun <T> List<List<T>>.findAll(t: T): List<Coord> =
+        flatMapIndexed { row, line ->
+            line.mapIndexedNotNull { col, v ->
+                if (v == t) row to col else null
+            }
+        }
+
+    private fun <T> List<T>.only(): T = this.also { check(it.size == 1) }.first()
+
+    private fun Coord.neighbors(numRows: Int, numCols: Int): List<Coord> =
+        mutableListOf<Coord>().also { result ->
+            (first - 1).let { if (it >= 0) result.add(copy(first = it)) }
+            (first + 1).let { if (it < numRows) result.add(copy(first = it)) }
+            (second - 1).let { if (it >= 0) result.add(copy(second = it)) }
+            (second + 1).let { if (it < numCols) result.add(copy(second = it)) }
+        }
+
+    private fun chartTerrain(terrain: List<List<Char>>): List<List<Int?>> {
         val numRows = terrain.size
         val numCols = terrain.first().size
         check(terrain.all { it.size == numCols })
-        val ends = terrain.flatMapIndexed { row, line ->
-            line.mapIndexedNotNull { col, char ->
-                if (char == 'E') row to col else null
-            }
-        }
-        check(ends.size == 1)
-        val end = ends.first()
-        val starts = terrain.flatMapIndexed { row, line ->
-            line.mapIndexedNotNull { col, char ->
-                if (char == 'a' - 1) row to col else null
-            }
-        }
-        check(starts.size == 1)
-        val start = starts.first()
+        val end = terrain.findAll('E').only()
 
         val pathState: MutableList<MutableList<Int?>> = MutableList(numRows) { MutableList(numCols) { null } }
         pathState[end] = 0
-        val toVisit: Queue<Pair<Int, Int>> = LinkedList()
+        val toVisit: Queue<Coord> = LinkedList()
         toVisit.add(end)
         while (toVisit.isNotEmpty()) {
             val visiting = toVisit.remove()
             val nextHeight = (terrain[visiting]!! - 1).let { if (it == 'D') 'z' else it }
             val nextValue = checkNotNull(pathState[visiting]) + 1
-            val nextVisits = mutableListOf<Pair<Int, Int>>()
-            visiting.copy(first = visiting.first - 1).let { if (it.first >= 0) nextVisits.add(it) }
-            visiting.copy(first = visiting.first + 1).let { if (it.first < numRows) nextVisits.add(it) }
-            visiting.copy(second = visiting.second - 1).let { if (it.second >= 0) nextVisits.add(it) }
-            visiting.copy(second = visiting.second + 1).let { if (it.second < numCols) nextVisits.add(it) }
-            nextVisits.forEach { coord ->
+            visiting.neighbors(numRows, numCols).forEach { coord ->
                 if (pathState[coord] == null && terrain[coord]!! >= nextHeight) {
                     pathState[coord] = nextValue
                     toVisit.add(coord)
@@ -55,12 +56,23 @@ class Day12 : Solution<Int> {
             }
         }
 
-//        pathState.forEach { line -> println(line.joinToString(" ") { it.toString().padStart(2, '0') }) }
-
-        pathState[start]!!
+//        pathState.forEach { line -> println(line.joinToString(" ") { (it ?: -1).toString().padStart(2, '0') }) }
+        return pathState
     }
 
-    override val part2 = SolutionPart { 0 }
+    override val part1 = SolutionPart(TEST_OUTPUT) { lines ->
+        val terrain = lines.map { it.map { c -> if (c == 'S') 'a' - 1 else c } }
+        val start = terrain.findAll('a' - 1).only()
+        chartTerrain(terrain)[start]!!
+    }
+
+    override val part2 = SolutionPart(TEST_OUTPUT_2) { lines ->
+        val terrain = lines.map { it.map { c -> if (c == 'S') 'a' - 1 else c } }
+        val chart = chartTerrain(terrain)
+        // this is lazy - chartTerrain is greedy; we could short-circuit
+        // upon finding an 'a' but that would require more work to write
+        terrain.findAll('a').asSequence().mapNotNull { chart[it] }.min()
+    }
 }
 
 fun main() = solutionMain<Day12>()
